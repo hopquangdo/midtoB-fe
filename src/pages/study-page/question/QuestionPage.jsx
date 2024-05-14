261ac31ab2ebfff5e8b46a74adea0fd10a66023a
unknown <dqhit999@gmail.com>
2024-05-14 18:57:12 +0700

    fix

--- src/pages/study-page/question/QuestionPage.jsx
+++ src/pages/study-page/question/QuestionPage.jsx
@@ -59,10 +59,10 @@ const QuestionPage = () => {
                 <div className="flex flex-col items-center">
                     <div className="flex flex-row items-center p-5 justify-between border-b border-gray-300 w-full overflow-x-auto">
                         <div className="flex flex-row items-center">
-                            <svg class="w-6 h-6 text-gray-80 m-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <svg className="w-6 h-6 text-gray-80 m-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
                             </svg>
-                            <p class="text-2xl font-semibold text-gray-90">Câu hỏi gần đây</p>
+                            <p className="text-2xl font-semibold text-gray-90">Câu hỏi gần đây</p>
                         </div>
                         <FilterQuestion/>
                         <SortQuestion/>
--- src/pages/study-page/question/components/HeaderQuestion.jsx
+++ src/pages/study-page/question/components/HeaderQuestion.jsx
@@ -15,7 +15,7 @@ const HeaderQuestion = () => {
                 </svg>
                 <input placeholder="Tìm kiếm..." className="p-1 bg-transparent focus:bg-none"/>
             </div>
-            <Link to={"/study/questions/ask"}  class="inline-block rounded bg-red-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-neutral-700 hover:shadow-dark-2 focus:bg-neutral-700 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-dark-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+            <Link to={"/study/questions/ask"}  className="inline-block rounded bg-red-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-neutral-700 hover:shadow-dark-2 focus:bg-neutral-700 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-dark-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
                 Đặt câu hỏi
             </Link>
         </div>
